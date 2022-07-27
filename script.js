486635a91915c10db246038a9333c3c633e79406
Ngawang Luding <ngluding@gmail.com>
2022-07-27 00:40:32 -0400

    Added html and css to build calculator

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const numberButton = document.querySelectorAll("[data-number]")
+const operationButton = document.querySelectorAll("[data-operation]")
+const equalsButton = document.querySelector("[data-equals]")
+const clearButton = document.querySelector("[data-clear]")
+const deleteButton = document.querySelecto("[data-delete]")
+const prevOperandTextElement = document.querySelector("[data-prev-operand]")
+const currentOperandTextElement= document.querySelector("[data-current-operand]")
+
+
 function add(num1, num2) {
     return num1 + num2;
 };
@@ -13,4 +22,10 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 };
+
+function operate(operator, num1, num2) {
+    
+
+};
+
 console.log(divide(10,5));
