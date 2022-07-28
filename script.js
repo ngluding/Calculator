a48ef5363983a9d39434fc6104436784dffbbfa0
Ngawang Luding <ngluding@gmail.com>
2022-07-28 02:56:28 -0400

    Updated javascript to display number of clicked button and limit to only one decimal

--- script.js
+++ script.js
@@ -2,10 +2,40 @@ const numberButton = document.querySelectorAll("[data-number]")
 const operationButton = document.querySelectorAll("[data-operation]")
 const equalsButton = document.querySelector("[data-equals]")
 const clearButton = document.querySelector("[data-clear]")
-const deleteButton = document.querySelecto("[data-delete]")
+const deleteButton = document.querySelector("[data-delete]")
 const prevOperandTextElement = document.querySelector("[data-prev-operand]")
 const currentOperandTextElement= document.querySelector("[data-current-operand]")
 
+let currentNum = "";
+
+numberButton.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        updateDisplay(e.target.textContent);
+        console.log(e);
+    });
+});
+
+operationButton.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        updateDisplay(e.target.textcontent);
+        console.log(e);
+    })
+})
+
+function updateDisplay(number) {
+    if (number === "." && currentOperandTextElement.textContent.includes(".")) return;
+    
+    currentNum += number;
+    currentOperandTextElement.textContent = currentNum;
+
+}
+
+function clear() {
+
+
+    currentOperandTextElement = "";
+    prevOperandTextElement = "";
+}
 
 function add(num1, num2) {
     return num1 + num2;
@@ -27,5 +57,3 @@ function operate(operator, num1, num2) {
     
 
 };
-
-console.log(divide(10,5));
